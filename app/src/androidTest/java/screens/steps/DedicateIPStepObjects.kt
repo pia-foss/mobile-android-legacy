b675992f486a0f3c04187b0ba6eb63b1d9a6447c
kp-ruth-o <ruth.ondrada@kape.com>
2023-10-12 19:22:36 +0800

    PIA-634:Create a test for DIP field for logout
    Created DedicatedIPPageObjects and Step objects to access DIP page. This test will check that
    the DIP get reset when user logout of the application

---
+++ app/src/androidTest/java/screens/steps/DedicateIPStepObjects.kt
@@ -0,0 +1,19 @@
+package com.privateinternetaccess.android.screens.steps
+
+import com.privateinternetaccess.android.BuildConfig
+import com.privateinternetaccess.android.helpers.ActionHelpers.inputTextInField
+import com.privateinternetaccess.android.screens.objects.DedicateIPPageObjects
+
+class DedicateIPStepObjects {
+
+    val dedicateIPPageObjects = DedicateIPPageObjects()
+
+    fun enterDedicateIP(DIPToken : String = BuildConfig.PIA_VALID_DIP_TOKEN ) {
+        inputTextInField(dedicateIPPageObjects.dedicateIPField, DIPToken)
+    }
+
+    fun activateButton() {
+        dedicateIPPageObjects.activateButton.click()
+    }
+
+}
