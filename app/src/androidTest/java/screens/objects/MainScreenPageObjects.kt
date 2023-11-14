2603b66f5226de55f185ccdb2752abaf2c543dd4
kp-ruth-o <143588152+kp-ruth-o@users.noreply.github.com>
2023-11-14 14:20:53 +0800

    PIA-636: Add new test for ServerLists (#46)
    Add new test for serverlist that checks for valid region, invalid region and connecting from serverlist page.

--- app/src/androidTest/java/screens/objects/MainScreenPageObjects.kt
+++ app/src/androidTest/java/screens/objects/MainScreenPageObjects.kt
@@ -8,4 +8,7 @@ class MainScreenPageObjects {
     val hamburgerMenu = LocatorHelper.findByContentDesc("Open")
     val quickConnectFlag1 = LocatorHelper.findByResourceId("com.privateinternetaccess.android:id/quick_server_flag_1")
     val quickConnectText1 = LocatorHelper.findByResourceId("com.privateinternetaccess.android:id/quick_server_name_1")
+    val currentRegion = LocatorHelper.findByResourceId("com.privateinternetaccess.android:id/fragment_connect_flag_area")
+    val regionNameSelected = LocatorHelper.findByResourceId("com.privateinternetaccess.android:id/fragment_connect_server_name")
+
 }
